145e5665290ba4e1512ac33a931f8dcb6b1ab192
Trebuh <hubertsosnicki2000@gmail.com>
2020-10-29 11:13:08 +0100

    up

--- src/services/Payu.php
+++ src/services/Payu.php
@@ -276,7 +276,12 @@ class Payu extends Component
         // $obj["card"] = json_decode($response)->cardTokens[0]->value;
         // $obj["accessToken"] = $accessToken;
 
-        return array(json_decode($response)->cardTokens[0]->value, $accessToken);
+        if ($token = json_decode($response)->cardTokens) {
+            return array($token[0]->value, $accessToken);
+        } else {
+            return array(false, $accessToken);
+        }
+
     }
 
     // TODO: Cron co 0.5h
@@ -291,20 +296,25 @@ class Payu extends Component
             // TODO: Jeśli minęło 30 dni od lastNotification w tabeli recurring to wysyłamy maila z linkiem do anulowania subskrybcji.
             // Siema, za 7 dni przeprowadzimy płątnośc za tyle i tyle szekli na to i to, jesli chcesz anulować to kliknij se tu.
             if(strtotime($recurringPayment['lastNotification']) < strtotime('-30 days')) {
-                $accessToken = $this->getCardToken($recurringPayment, $endpointDomain)[0];
-                $model = Recurring::findOne(["id" => $recurringPayment["id"]]);
-                $model->setAttribute("lastNotification", date("Y-m-d H:i:s"));
-                $model -> setAttribute('cancelHash', uniqid(uniqid(), true));
-                $model->save();
-                Payments::instance()->sendEmail($email, true, "\nChcesz anulowac subskrypcję? Kliknij w link! ".Craft::$app->config->general->cancelSubscription."?id=".$model->cancelHash, $recurringPayment["firstName"], $recurringPayment["project"]);
+                if ($accessToken = $this->getCardToken($recurringPayment, $endpointDomain)[0]) {
+                    // $accessToken = $tokens[0];
+                    $model = Recurring::findOne(["id" => $recurringPayment["id"]]);
+                    $model->setAttribute("lastNotification", date("Y-m-d H:i:s"));
+                    $model -> setAttribute('cancelHash', uniqid(uniqid(), true));
+                    $model->save();
+                    Payments::instance()->sendEmail($email, true, "\nChcesz anulowac subskrypcję? Kliknij w link! ".Craft::$app->config->general->cancelSubscription."?id=".$model->cancelHash, $recurringPayment["firstName"], $recurringPayment["project"]);
+                }
             }
+            // print("ERROR".$recurringPayment["id"]);
             // Tu po pobraniu tokenów będzie płatność cykliczna.
             if(strtotime($recurringPayment['lastPayment']) < strtotime('-30 days')) {
-                $accessToken = $this->getCardToken($recurringPayment, $endpointDomain)[0];
-                $model = Recurring::findOne(["id" => $recurringPayment["id"]]);
-                $this->makePayment(false, $model, true, $accessToken, "CARD_TOKEN", false);
-                $model->setAttribute("lastPayment", date("Y-m-d H:i:s"));
-                $model->save();
+                if ($accessToken = $this->getCardToken($recurringPayment, $endpointDomain)[0]) {
+                    // $accessToken = $tokens;
+                    $model = Recurring::findOne(["id" => $recurringPayment["id"]]);
+                    $this->makePayment(false, $model, true, $accessToken, "CARD_TOKEN", false);
+                    $model->setAttribute("lastPayment", date("Y-m-d H:i:s"));
+                    $model->save();
+                }
             }
         }
     }
